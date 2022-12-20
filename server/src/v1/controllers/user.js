a8c577848e56bd080f0993a8e90fa49f1b73ee86
Mahendra <mahendrabrowny@gmail.com>
2022-12-20 22:19:40 +0530

    user controller functions for login signup 🛠

---
+++ server/src/v1/controllers/user.js
@@ -0,0 +1,72 @@
+const User = require("../models/user");
+const CryptoJS = require("crypto-js");
+const jsonwebtoken = require("jsonwebtoken");
+
+// REGISTER
+export const register = async (req, res) => {
+  const { password } = req.body;
+  try {
+    req.body.password = CryptoJS.AES.encrypt(
+      password,
+      process.env.PASSWORD_SECRET
+    );
+
+    const user = await User.create(req.body);
+    const token = jsonwebtoken.sign(
+      { id: user._id },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    res.status(201).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+//LOGIN
+export const login = async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const user = await User.findById({ username }).select("password username");
+
+    if (!user) {
+      return res.status(401).json({
+        errors: [
+          {
+            param: "username",
+            msg: "Invalid username or password!",
+          },
+        ],
+      });
+    }
+
+    const decryptedPas = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASSWORD_SECRET
+    ).toString(CryptoJS.enc.Utf8);
+
+    // If Password Doesn't Match
+    if (decryptedPas !== password) {
+      return res.status(401).json({
+        errors: [
+          {
+            param: "username",
+            msg: "Invalid username or password!",
+          },
+        ],
+      });
+    }
+
+    user.password = undefined;
+    const token = jsonwebtoken.sign(
+      { id: user._id },
+      process.env.TOKEN_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    res.status(200).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
