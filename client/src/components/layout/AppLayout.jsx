75c71dc0ac373129064ae226a53456b57609b536
Mahendra <mahendrabrowny@gmail.com>
2022-12-21 20:33:44 +0530

    protected routes 🙂👍🏻

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,11 +1,35 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import { Outlet, useNavigate } from "react-router-dom";
+import authUtils from "../../utils/authUtils";
+import Loader from "../common/Loader";
+import Sidebar from "../common/Sidebar";
 
 const AppLayout = () => {
-  return (
-    <div>
-      <Outlet />
-    </div>
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const user = await authUtils.isAuthenticated();
+      if (!user) {
+        navigate("/login");
+      } else {
+        setLoading(false);
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  return loading ? (
+    <Loader fullHeight />
+  ) : (
+    <Box sx={{ display: "flex" }}>
+      <Sidebar />
+      <Box sx={{ flexGrow: 1, p: 1, width: "max-content" }}>
+        <Outlet />
+      </Box>
+    </Box>
   );
 };
 
--- client/src/components/layout/AuthLayout.jsx
+++ client/src/components/layout/AuthLayout.jsx
@@ -1,11 +1,48 @@
-import React from "react";
-import { Outlet } from "react-router-dom";
+import { Box, Container } from "@mui/material";
+import { useEffect } from "react";
+import { useState } from "react";
+import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import authUtils from "../../utils/authUtils";
+import Loader from "../common/Loader";
+import assets from "../../assets";
+import Sidebar from "../common/Sidebar";
 
 const AuthLayout = () => {
-  return (
-    <div>
-      <Outlet />
-    </div>
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const checkAuth = async () => {
+      const user = await authUtils.isAuthenticated();
+      if (!user) {
+        setLoading(false);
+      } else {
+        navigate("/");
+      }
+    };
+    checkAuth();
+  }, [navigate]);
+
+  return loading ? (
+    <Loader fullHeight />
+  ) : (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <img
+          src={assets.images.logoDark}
+          style={{ width: "100px" }}
+          alt="logo"
+        />
+        <Outlet />
+      </Box>
+    </Container>
   );
 };
 
