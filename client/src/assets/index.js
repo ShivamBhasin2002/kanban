9a73fd80ca230fd971c678c27bce9aebb6360321
Mahendra <mahendrabrowny@gmail.com>
2022-12-21 20:33:23 +0530

    assests image erro fix

--- client/src/assets/index.js
+++ client/src/assets/index.js
@@ -1,7 +1,10 @@
+import darkLogo from "./images/logo-dark.png";
+import appIcon from "./images/favicon.png";
+
 const assets = {
   images: {
-    logoDark: require("./images/logo-dark.png"),
-    appIcon: require("./images/favicon.png"),
+    logoDark: darkLogo,
+    appIcon: appIcon,
   },
   colors: {
     secondary: "#292929",
