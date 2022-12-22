fb3f8d4c81bb3d1f240ac70afc436fd98456cd3e
Mahendra <mahendrabrowny@gmail.com>
2022-12-22 21:59:14 +0530

    todo: signup page logic

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,25 +1,87 @@
 import LoadingButton from "@mui/lab/LoadingButton";
 import { Box, Button, TextField } from "@mui/material";
+import axios from "axios";
 import { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import authApi from "../api/authApi";
 
 const SignUp = () => {
-  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const [loading, setLoading] = useState(false);
+  const [usernameErrText, setUsernameErrText] = useState("");
+  const [passwordErrText, setPasswordErrText] = useState("");
+  const [confirmPasswordErrText, setConfirmPasswordErrText] = useState("");
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
+
+    setPasswordErrText("");
+    setConfirmPasswordErrText("");
+
+    const data = new FormData(e.target);
+    const username = data.get("username").trim();
+    const password = data.get("password").trim();
+    const confirmPassword = data.get("confirmPassword").trim();
+
+    let err = false;
+
+    if (username === "") {
+      err = true;
+      setUsernameErrText("Please fill this field");
+    }
+    if (password === "") {
+      err = true;
+      setPasswordErrText("Please fill this field");
+    }
+    if (confirmPassword === "") {
+      err = true;
+      setConfirmPasswordErrText("Please fill this field");
+    }
+    if (password !== confirmPassword) {
+      err = true;
+      setConfirmPasswordErrText("Confirm password not match");
+    }
+
+    if (err) return;
+
+    setLoading(true);
+
+    try {
+      const res = await authApi.signup({
+        username,
+        password,
+        confirmPassword,
+      });
+      setLoading(false);
+      localStorage.setItem("token", res.token);
+      navigate("/");
+    } catch (err) {
+      console.log(err.message);
+      // const errors = err.data?.errors;
+      // errors.forEach((e) => {
+      //   if (e.param === "username") {
+      //     setUsernameErrText(e.msg);
+      //   }
+      //   if (e.param === "password") {
+      //     setPasswordErrText(e.msg);
+      //   }
+      //   if (e.param === "confirmPassword") {
+      //     setConfirmPasswordErrText(e.msg);
+      //   }
+      // });
+      setLoading(false);
+    }
   };
+
+  // const handleSubmit = async (e) => {
+  //   e.preventDefault()
+  //    const response = axios.get('http://127.0.0.1:5000/api/v1/')
+  // }
 
   return (
     <>
-      <Box
-        component="form"
-        sx={{
-          mt: 1,
-        }}
-        onSubmit={handleSubmit}
-        noValidate
-      >
+      <Box component="form" sx={{ mt: 1 }} onSubmit={handleSubmit} noValidate>
         <TextField
           margin="normal"
           required
@@ -28,26 +90,32 @@ const SignUp = () => {
           label="Username"
           name="username"
           disabled={loading}
+          error={usernameErrText !== ""}
+          helperText={usernameErrText}
         />
         <TextField
           margin="normal"
           required
           fullWidth
           id="password"
-          type="password"
           label="Password"
           name="password"
+          type="password"
           disabled={loading}
+          error={passwordErrText !== ""}
+          helperText={passwordErrText}
         />
         <TextField
           margin="normal"
           required
           fullWidth
           id="confirmPassword"
-          type="password"
           label="Confirm Password"
           name="confirmPassword"
+          type="password"
           disabled={loading}
+          error={confirmPasswordErrText !== ""}
+          helperText={confirmPasswordErrText}
         />
         <LoadingButton
           sx={{ mt: 3, mb: 2 }}
@@ -57,11 +125,11 @@ const SignUp = () => {
           type="submit"
           loading={loading}
         >
-          SignUp
+          Signup
         </LoadingButton>
       </Box>
       <Button component={Link} to="/login" sx={{ textTransform: "none" }}>
-        Already have an account? Login Here
+        Already have an account? Login
       </Button>
     </>
   );
