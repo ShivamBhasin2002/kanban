b9da98b08b3a2130abd20fa03c67fe650023d8b8
Mahendra <mahendrabrowny@gmail.com>
2022-12-22 22:00:23 +0530

    function name fix

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -2,7 +2,7 @@ import axiosClient from "./axiosClient";
 
 const authApi = {
   signup: (params) => axiosClient.post("auth/signup", params),
-  signup: (params) => axiosClient.post("auth/login", params),
+  login: (params) => axiosClient.post("auth/login", params),
   verifyToken: () => axiosClient.post("auth/verify-token"),
 };
 
