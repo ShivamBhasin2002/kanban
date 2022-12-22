f2a16f43e79f92548ece6a0310ff2bc5e9cbe72a
Mahendra <mahendrabrowny@gmail.com>
2022-12-22 08:47:59 +0530

    Login page UI done ✔

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,60 @@
-import React from "react";
+import LoadingButton from "@mui/lab/LoadingButton";
+import { Box, Button, TextField } from "@mui/material";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  const [loading, setLoading] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <>
+      <Box
+        component="form"
+        sx={{
+          mt: 1,
+        }}
+        onSubmit={handleSubmit}
+        noValidate
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="username"
+          label="Username"
+          name="username"
+          disabled={loading}
+        />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="password"
+          type="password"
+          label="Password"
+          name="password"
+          disabled={loading}
+        />
+        <LoadingButton
+          sx={{ mt: 3, mb: 2 }}
+          variant="outlined"
+          fullWidth
+          color="success"
+          type="submit"
+          loading={loading}
+        >
+          Login
+        </LoadingButton>
+      </Box>
+      <Button component={Link} to="/signup" sx={{ textTransform: "none" }}>
+        Don't have an account? SignUp
+      </Button>
+    </>
+  );
 };
 
 export default Login;
