51fae50a3d7fe3a6f4fe134149f8e6ebc8e15229
Mahendra <mahendrabrowny@gmail.com>
2023-02-27 17:57:22 +0530

    dispatch user login actions

--- client/src/components/layout/AppLayout.jsx
+++ client/src/components/layout/AppLayout.jsx
@@ -1,12 +1,15 @@
 import { Box } from "@mui/material";
 import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
 import { Outlet, useNavigate } from "react-router-dom";
+import { setUser } from "../../features/user/userSlice";
 import authUtils from "../../utils/authUtils";
 import Loader from "../common/Loader";
 import Sidebar from "../common/Sidebar";
 
 const AppLayout = () => {
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -15,6 +18,7 @@ const AppLayout = () => {
       if (!user) {
         navigate("/login");
       } else {
+        dispatch(setUser(user));
         setLoading(false);
       }
     };
