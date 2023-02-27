6f7e81a3f9a2c8f0d3646e246258549e2b66f5d9
Mahendra <mahendrabrowny@gmail.com>
2023-02-27 17:37:48 +0530

    configure redux-toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { ToastContainer } from "react-toastify";
 import App from "./App";
+import { store } from "./features/store";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
     <ToastContainer
       position="top-center"
       autoClose={3200}
