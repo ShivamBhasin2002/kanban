f073e3cd968c15e0639aadda6d0d7e685ff956df
Mahendra <mahendrabrowny@gmail.com>
2023-02-27 17:57:59 +0530

    create userSlice features

--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,5 +1,18 @@
-import {} from "@reduxjs/toolkit";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
   value: {},
 };
+
+export const userSlice = createSlice({
+  name: "user",
+  initialState,
+  reducers: {
+    setUser: (state, action) => {
+      state.value = action.payload;
+    },
+  },
+});
+
+export const { setUser } = userSlice.actions;
+export default userSlice.reducer;
