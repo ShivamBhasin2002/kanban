728c3f825f73f8c1514581e6426ad060dbc1195f
Mahendra <mahendrabrowny@gmail.com>
2023-02-27 17:30:24 +0530

    configure react-tostify

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ToastContainer } from "react-toastify";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
+    <ToastContainer
+      position="top-center"
+      autoClose={3200}
+      newestOnTop={false}
+      closeOnClick
+    />
   </React.StrictMode>
 );
