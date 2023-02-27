6939629e13a7b4f828210248ab1b6d1dae15a2c1
Mahendra <mahendrabrowny@gmail.com>
2023-02-27 17:30:08 +0530

    toastify css file

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "react-toastify/dist/ReactToastify.css";
 import { CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
