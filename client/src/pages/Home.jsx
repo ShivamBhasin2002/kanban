f580461c56fd2987edb7d128d16702062859663c
Mahendra <mahendrabrowny@gmail.com>
2023-03-01 17:27:13 +0530

    create board functionality done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,19 +1,43 @@
-import { Box } from "@mui/material";
 import LoadingButton from "@mui/lab/LoadingButton";
+import { Box } from "@mui/material";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import boardApi from "../api/boardApi";
 
 const Home = () => {
-  const createBoard = () => {};
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+
+  const createBoard = () => {
+    setLoading(true);
+    try {
+      const res = boardApi.createBoard();
+      navigate(`/boards/${res.id}`);
+    } catch (err) {
+      toast.error(err.message);
+    } finally {
+      setLoading(false);
+    }
+  };
 
   return (
     <Box
       sx={{
-        height: "100%",
+        height: "100vh",
         display: "flex",
         alignItems: "center",
-        justifyCenter: "center",
+        justifyContent: "center",
       }}
     >
-      <LoadingButton variant="outlined" color="success" onClick={createBoard}>
+      <LoadingButton
+        variant="outlined"
+        color="success"
+        onClick={createBoard}
+        loading={loading}
+      >
         Click here to create your first Board
       </LoadingButton>
     </Box>
