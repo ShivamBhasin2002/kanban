92e67094a82b7137f69f42f5a5c8bea9e11f610e
Mahendra <mahendrabrowny@gmail.com>
2023-03-01 17:27:27 +0530

    fetch boards

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,17 +1,40 @@
-import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import AddBoxOutlined from "@mui/icons-material/AddBoxOutlined";
+import LogoutOutlinedIcon from "@mui/icons-material/LogoutOutlined";
 import { Drawer, IconButton, List, ListItem, Typography } from "@mui/material";
 import { Box } from "@mui/system";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import boardApi from "../../api/boardApi";
 import assests from "../../assets/index";
+import { setBoard } from "../../features/board/boardSlice";
 
 const Sidebar = () => {
-  const dipatch = useDispatch();
+  const dispatch = useDispatch();
+  const boards = useSelector((state) => state.board.value);
   const navigate = useNavigate();
   const user = useSelector((state) => state.user.value);
   const sidebarWidth = 250;
+
+  const getAllBoards = async () => {
+    try {
+      const res = await boardApi.getAllBoards();
+      dispatch(setBoard([res]));
+    } catch (err) {
+      toast.error(err.message);
+      console.log(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getAllBoards();
+  }, []);
+
+  useEffect(() => {
+    console.log("userEffectBoard" + boards);
+  }, [boards]);
+
   const logout = () => {
     localStorage.removeItem("token");
     navigate("/login");
