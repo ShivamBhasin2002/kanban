41c485a1fe3c68974beecaaab76ce775458e0114
Mahendra <mahendrabrowny@gmail.com>
2023-03-02 21:58:58 +0530

    get single board routes

--- server/src/v1/routes/board.js
+++ server/src/v1/routes/board.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-const { body } = require("express-validator");
+const { param } = require("express-validator");
 const validation = require("../handlers/validation");
 const tokenHandler = require("../handlers/tokenHandle");
 const boardController = require("../controllers/board");
@@ -7,5 +7,15 @@ const boardController = require("../controllers/board");
 router.post("/", tokenHandler.verifyToken, boardController.createBoard);
 router.get("/", tokenHandler.verifyToken, boardController.getAllBoards);
 router.put("/", tokenHandler.verifyToken, boardController.updateBoardPosition);
+router.get(
+  "/:boardId",
+  param("boardId").custom((value) => {
+    if (!validation.isObjectId(value)) {
+      return Promise.reject("Invalid Id!");
+    }
+  }),
+  tokenHandler.verifyToken,
+  boardController.getSingleBoard
+);
 
 module.exports = router;
