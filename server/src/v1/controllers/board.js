a1730c0e8e2279934b2f02d1a6275e13d6c8f4be
Mahendra <mahendrabrowny@gmail.com>
2023-03-02 21:59:10 +0530

    get single board controller function

--- server/src/v1/controllers/board.js
+++ server/src/v1/controllers/board.js
@@ -1,6 +1,7 @@
 const Board = require("../models/board");
 const Task = require("../models/task");
 const Section = require("../models/section");
+const board = require("../models/board");
 
 exports.createBoard = async (req, res) => {
   try {
@@ -32,6 +33,26 @@ exports.updateBoardPosition = async (req, res) => {
       await Board.findByIdAndUpdate(board?.id, { $set: { position: key } });
     }
     res.status(200).json("Board Position Updated!");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
+
+exports.getSingleBoard = async (req, res) => {
+  const { boardId } = req.params;
+  try {
+    const board = await Board.findOne({ user: req.user._id, _id: boardId });
+    if (!board) return res.status(400).json("No Board found with this ID!");
+
+    const sections = await Section.find({ board: boardId });
+    for (const section of sections) {
+      const tasks = await Task.find({ section: section.id })
+        .populate("section")
+        .sort("-position");
+      section._doc.tasks = tasks;
+    }
+    board._doc.sections = sections;
+    res.status(200).json(board);
   } catch (err) {
     res.status(500).json(err.message);
   }
