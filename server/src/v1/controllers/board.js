5e33d1a9fbce816ff0a2ed8783759f8d0d66ee06
Mahendra <mahendrabrowny@gmail.com>
2023-03-02 18:37:50 +0530

    updateBoard controller function

--- server/src/v1/controllers/board.js
+++ server/src/v1/controllers/board.js
@@ -23,3 +23,16 @@ exports.getAllBoards = async (req, res) => {
     res.status(500).json(err.message);
   }
 };
+
+exports.updateBoardPosition = async (req, res) => {
+  const { boards } = req.body;
+  try {
+    for (const key in boards.reverse()) {
+      const board = boards[key];
+      await Board.findByIdAndUpdate(board?.id, { $set: { position: key } });
+    }
+    res.status(200).json("Board Position Updated!");
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
+};
