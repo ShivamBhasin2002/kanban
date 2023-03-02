373738137fe75f2d0b5c486a07386ac9fae1a89c
Mahendra <mahendrabrowny@gmail.com>
2023-03-02 18:38:10 +0530

    updateBoard client side function

--- client/src/api/boardApi.js
+++ client/src/api/boardApi.js
@@ -4,6 +4,7 @@ import axiosClient from "./axiosClient";
 const boardApi = {
   createBoard: () => axiosClient.post("boards"),
   getAllBoards: () => axiosClient.get("boards"),
+  updateBoardPosition: (params) => axiosClient.put("boards", params),
 };
 
 export default boardApi;
