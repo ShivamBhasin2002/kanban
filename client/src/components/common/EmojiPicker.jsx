3bccb18ccb37c3390dd158471157dfd6177976fc
Mahendra <mahendrabrowny@gmail.com>
2023-03-05 09:40:55 +0530

    emoji picker component completed ✅

--- client/src/components/common/EmojiPicker.jsx
+++ client/src/components/common/EmojiPicker.jsx
@@ -1,7 +1,40 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { Picker } from "emoji-mart";
+import "emoji-mart/css/emoji-mart.css";
 
-const EmojiPicker = () => {
-  return <div>=</div>;
+const EmojiPicker = (props) => {
+  const [selectedEmoji, setSelectedEmoji] = useState();
+  const [isShowPicker, setIsShowPicker] = useState(false);
+
+  useEffect(() => {
+    setSelectedEmoji(props.icon);
+  }, [props.icon]);
+
+  const showPicker = () => setIsShowPicker(!isShowPicker);
+
+  return (
+    <Box sx={{ position: "relative", width: "max-content" }}>
+      <Typography
+        variant="h3"
+        fontWeight="700"
+        sx={{ cursor: "pointer" }}
+        onClick={showPicker}
+      >
+        {selectedEmoji}
+      </Typography>
+      <Box
+        sx={{
+          display: isShowPicker ? "block" : "none",
+          position: "absolute",
+          top: "100%",
+          zIndex: "999",
+        }}
+      >
+        <Picker theme="dark" onSelect={selectedEmoji} showPreview={false} />
+      </Box>
+    </Box>
+  );
 };
 
 export default EmojiPicker;
