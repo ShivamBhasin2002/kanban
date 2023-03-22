4c2696b3b65415019b8635776903bb23e8dcfba0
Mahendra <mahendrabrowny@gmail.com>
2023-03-22 17:58:49 +0530

    favourite board reducer

--- client/src/features/store.js
+++ client/src/features/store.js
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../features/user/userSlice";
 import boardReducer from "../features/board/boardSlice";
+import favouriteReducer from "../features/board/favouriteSlice";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
     board: boardReducer,
+    favourites: favouriteReducer,
   },
 });
